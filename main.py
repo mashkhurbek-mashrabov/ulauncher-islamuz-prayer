06ff951c7f0b1847f0d0722e5fb23e41e696d88a
Mashkhurbek <mashkhurbek.mashrabov@gmail.com>
2024-02-08 23:01:13 +0500

    optimized

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from ulauncher.api.client.Extension import Extension
 from ulauncher.api.client.EventListener import EventListener
+from ulauncher.api.shared.action.HideWindowAction import HideWindowAction
 from ulauncher.api.shared.event import KeywordQueryEvent
 from ulauncher.api.shared.action.RenderResultListAction import RenderResultListAction
+from ulauncher.api.shared.item.ExtensionResultItem import ExtensionResultItem
 
-from prayer import Prayer
+from prayer import Language, PrayerService
 
 
 class PrayerExtension(Extension):
@@ -18,8 +20,36 @@ class KeywordQueryEventListener(EventListener):
     def on_event(self, event, extension):
         location = extension.preferences["location"]
         language = extension.preferences["language"]
-        prayer = Prayer(location, language)
-        return RenderResultListAction(prayer.get_result_list())
+
+        if language not in [Language.ENGLISH, Language.UZBEK]:
+            language = Language.ENGLISH  # Default to English if an invalid language is selected
+
+        prayer_service = PrayerService(location, language)
+        prayer_times = prayer_service.get_prayer_times()
+
+        return RenderResultListAction([
+            ExtensionResultItem(icon='images/calendar.png',
+                                name=f"{prayer_times.get_prayer_name('hij_date', language)} : {prayer_times.get_hijri_date()}",
+                                on_enter=HideWindowAction()),
+            ExtensionResultItem(icon='images/fajr.png',
+                                name=f"{prayer_times.get_prayer_name('fajr', language)} : {prayer_times.get_time('tong_saharlik')}",
+                                on_enter=HideWindowAction()),
+            ExtensionResultItem(icon='images/sunrise.png',
+                                name=f"{prayer_times.get_prayer_name('sunrise', language)} : {prayer_times.get_time('quyosh')}",
+                                on_enter=HideWindowAction()),
+            ExtensionResultItem(icon='images/dhuhr.png',
+                                name=f"{prayer_times.get_prayer_name('dhuhr', language)} : {prayer_times.get_time('peshin')}",
+                                on_enter=HideWindowAction()),
+            ExtensionResultItem(icon='images/asr.png',
+                                name=f"{prayer_times.get_prayer_name('asr', language)} : {prayer_times.get_time('asr')}",
+                                on_enter=HideWindowAction()),
+            ExtensionResultItem(icon='images/maghrib.png',
+                                name=f"{prayer_times.get_prayer_name('maghrib', language)} : {prayer_times.get_time('shom_iftor')}",
+                                on_enter=HideWindowAction()),
+            ExtensionResultItem(icon='images/isha.png',
+                                name=f"{prayer_times.get_prayer_name('isha', language)} : {prayer_times.get_time('hufton')}",
+                                on_enter=HideWindowAction())
+        ])
 
 
 if __name__ == '__main__':
--- prayer.py
+++ prayer.py
@@ -1,130 +1,70 @@
 import requests
-from ulauncher.api.shared.action.HideWindowAction import HideWindowAction
-from ulauncher.api.shared.item.ExtensionResultItem import ExtensionResultItem
 
 
-class UserLanguage:
+class Language:
     ENGLISH = 'en'
     UZBEK = 'uz'
 
-    def get_lang(self, language: str):
-        if language == 'en':
-            return self.ENGLISH
-        elif language == 'uz':
-            return self.UZBEK
+
+# prayer_times.py (update)
+
+class PrayerTimes:
+    PRAYER_NAMES = {
+        Language.ENGLISH: {
+            'fajr': 'Fajr',
+            'sunrise': 'Sunrise',
+            'dhuhr': 'Dhuhr',
+            'asr': 'Asr',
+            'maghrib': 'Maghrib',
+            'isha': 'Isha',
+            'hij_date': "Hijri Date"
+        },
+        Language.UZBEK: {
+            'fajr': 'Bomdod',
+            'sunrise': 'Quyosh',
+            'dhuhr': 'Peshin',
+            'asr': 'Asr',
+            'maghrib': 'Shom',
+            'isha': 'Hufton',
+            'hij_date': "Hijriy Sana"
+        },
+    }
+
+    def __init__(self, times: dict):
+        self.times = times
+
+    def get_time(self, key: str) -> str:
+        return self.times['times'].get(key, "")
+
+    def get_hijri_date(self) -> str:
+        hijri_date = self.times.get('hijri_date', {})
+        day = hijri_date.get('day', '')
+        month = hijri_date.get('month', '').capitalize()
+        return f"{day} {month}"
+
+    def get_prayer_name(self, prayer_key: str, language: str) -> str:
+        language_prayers = self.PRAYER_NAMES.get(language, {})
+        return language_prayers.get(prayer_key.lower(), "")
 
 
-class Prayer:
-    def __init__(self, location: str, language: UserLanguage):
+class PrayerAPI:
+    BASE_URL = "https://islomapi.uz/api/present/day"
+
+    def __init__(self, location: str):
+        self.location = location
+
+    def get_prayer_times(self) -> dict:
+        response = requests.get(f"{self.BASE_URL}?region={self.location}")
+        response.raise_for_status()  # Raise an exception for non-200 responses
+        return response.json()
+
+
+class PrayerService:
+    def __init__(self, location: str, language: str):
         self.location = location
         self.language = language
-        self.fajr_time = ""
-        self.sunrise_time = ""
-        self.dhuhr_time = ""
-        self.asr_time = ""
-        self.maghrib_time = ""
-        self.isha_time = ""
-        self.date = ""
-        self.hij_date = ""
-        self.get_prayer_time()
 
-    def get_prayer_time(self):
-        response = requests.get(self.api_maker())
-        if response.status_code == 200:
-            data = response.json()
-            self.date = data['date']
-            self.hij_date = f"{data['hijri_date']['day']} {data['hijri_date']['month'].capitalize()}"
-            times = data["times"]
-            self.fajr_time = times['tong_saharlik']
-            self.sunrise_time = times['quyosh']
-            self.dhuhr_time = times['peshin']
-            self.asr_time = times['asr']
-            self.maghrib_time = times['shom_iftor']
-            self.isha_time = times['hufton']
-        else:
-            raise Exception("Request failed: %s" % response.status_code)
-
-    def get_fajr_name(self):
-        if self.language == UserLanguage.UZBEK:
-            return "Bomdod"
-        else:
-            return "Fajr"
-
-    def get_sunrise_name(self):
-        if self.language == UserLanguage.UZBEK:
-            return "Quyosh"
-        else:
-            return "Sunrise"
-
-    def get_dhuhr_name(self):
-        if self.language == UserLanguage.UZBEK:
-            return "Peshin"
-        else:
-            return "Dhuhr"
-
-    def get_asr_name(self):
-        if self.language == UserLanguage.UZBEK:
-            return "Asr"
-        else:
-            return "Asr"
-
-    def get_maghrib_name(self):
-        if self.language == UserLanguage.UZBEK:
-            return "Shom"
-        else:
-            return "Maghrib"
-
-    def get_isha_name(self):
-        if self.language == UserLanguage.UZBEK:
-            return "Hufton"
-        else:
-            return "Isha"
-
-    def get_hij_date_name(self):
-        if self.language == UserLanguage.UZBEK:
-            return "Hijriy sana"
-        else:
-            return "Hijri Date"
-
-    def api_maker(self):
-        return f"https://islomapi.uz/api/present/day?region={self.location}"
-
-    def get_result_list(self):
-        items = []
-
-        items.append(
-            ExtensionResultItem(icon='images/calendar.png',
-                                name=f"{self.get_hij_date_name()} : {self.hij_date}",
-                                on_enter=HideWindowAction()))
-
-        items.append(
-            ExtensionResultItem(icon='images/fajr.png',
-                                name=f"{self.get_fajr_name()} : {self.fajr_time}",
-                                on_enter=HideWindowAction()))
-
-        items.append(
-            ExtensionResultItem(icon='images/sunrise.png',
-                                name=f"{self.get_sunrise_name()} : {self.sunrise_time}",
-                                on_enter=HideWindowAction()))
-
-        items.append(
-            ExtensionResultItem(icon='images/dhuhr.png',
-                                name=f"{self.get_dhuhr_name()} : {self.dhuhr_time}",
-                                on_enter=HideWindowAction()))
-
-        items.append(
-            ExtensionResultItem(icon='images/asr.png',
-                                name=f"{self.get_asr_name()} : {self.asr_time}",
-                                on_enter=HideWindowAction()))
-
-        items.append(
-            ExtensionResultItem(icon='images/maghrib.png',
-                                name=f"{self.get_maghrib_name()} : {self.maghrib_time}",
-                                on_enter=HideWindowAction()))
-
-        items.append(
-            ExtensionResultItem(icon='images/isha.png',
-                                name=f"{self.get_isha_name()} : {self.isha_time}",
-                                on_enter=HideWindowAction()))
-
-        return items
+    def get_prayer_times(self) -> PrayerTimes:
+        api = PrayerAPI(self.location)
+        times_data = api.get_prayer_times()
+        return PrayerTimes(times_data)
